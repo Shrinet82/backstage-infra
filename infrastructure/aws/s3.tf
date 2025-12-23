79ea0e3f8336a9d3afce2a035dd1b0a519f5a70c
Shashwat Pratap SIngh <96904787+Shrinet82@users.noreply.github.com>
2025-12-23 08:22:24 +0000

    Terraform tempelate and github actions workflow added

---
+++ infrastructure/aws/s3.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # Terraform Cloud backend
+  cloud {
+    organization = "YOUR_TFC_ORG"
+    workspaces {
+      name = "backstage-aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "S3 bucket name (globally unique)"
+}
+
+variable "environment" {
+  type        = string
+  default     = "development"
+}
+
+resource "aws_s3_bucket" "backstage_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Backstage"
+  }
+}
+
+output "bucket_name" {
+  value       = aws_s3_bucket.backstage_bucket.id
+  description = "Name of the S3 bucket"
+}
